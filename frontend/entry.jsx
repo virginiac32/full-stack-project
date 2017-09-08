f095873780ced704330bb5326d4e13b7a806eeec
Virginia Chen <virginia.chen32@gmail.com>
2017-09-07 22:38:55 -0700

    Add to window for testing

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {fetchArtworks} from './util/artwork_api_util';
+import {receiveArtworks} from './actions/artwork_actions';
 
 import configureStore from './store/store';
 import Root from './components/root';
@@ -15,5 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.fetchArtworks = fetchArtworks;
+  window.receiveArtworks = receiveArtworks;
 
 });
